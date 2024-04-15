a6b69b1f3d8ed49932428945820af422f5c8980c
Fei <33122873+pheihuihui@users.noreply.github.com>
2024-04-15 23:33:14 +0800

    ...

--- src/background.ts
+++ src/background.ts
@@ -4,17 +4,25 @@ const MENU_SAVING_CONTENT = "menu-saving-content"
 
 chrome.runtime.onInstalled.addListener(function () {
     chrome.contextMenus.create({
-        contexts: ["page", "frame", "image"],
+        contexts: ["page", "image"],
         id: MENU_SAVING_CONTENT,
         title: "Save Content",
     })
 })
 
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {})
+
 chrome.contextMenus.onClicked.addListener(async function (info, tab) {
     if (info.menuItemId == MENU_SAVING_CONTENT) {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
             if (tabs && tabs[0] && tabs[0].id) {
-                chrome.tabs.sendMessage(tabs[0].id, { type: "saving-image", src: info.srcUrl })
+                console.log(info)
+                if (info.mediaType && info.mediaType == "image") {
+                    chrome.tabs.sendMessage(tabs[0].id, { type: "saving-content", contentType: "image", data: info.srcUrl })
+                } else {
+                    let data = await getContentFromCurrentPage(tabs[0].id)
+                    chrome.tabs.sendMessage(tabs[0].id, { type: "saving-content", contentType: "page", data: data })
+                }
             }
         })
     }
@@ -24,8 +32,8 @@ async function getContentFromCurrentPage(tabid: number) {
     return new Promise<string>((resolve, reject) => {
         chrome.pageCapture.saveAsMHTML({ tabId: tabid }, async (data) => {
             if (data?.size) {
-                // let txt = await data.text()
-                resolve(data.size.toString())
+                let txt = await data.text()
+                resolve(txt)
             } else {
                 reject("no data")
             }
--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,36 @@
-let port = chrome.runtime.connect({ name: "contentscript" })
-
-port.onMessage.addListener(function (msg) {
-    console.log(msg)
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.type && message.type == "saving-content") {
+        if (message.contentType && message.contentType == "image") {
+            fetch(message.data)
+                .then(async (response) => {
+                    let blob = await response.blob()
+                    let base64 = await blob2base64(blob)
+                    let obj = { type: "saving-content", contentType: "image", data: base64 }
+                    let str = JSON.stringify(obj)
+                    navigator.clipboard.writeText(str)
+                })
+                .then(() => {
+                    window.open("http://localhost:8080/desq/#/new-image")
+                })
+        } else if (message.contentType && message.contentType == "page") {
+            let obj = { type: "saving-content", contentType: "page", data: message.data, link: document.URL }
+            let str = JSON.stringify(obj)
+            navigator.clipboard.writeText(str).then(() => {
+                window.open("http://localhost:8080/desq/#/new-page")
+            })
+        }
+    }
 })
 
-port.postMessage("hello")
+function blob2base64(blob: Blob) {
+    return new Promise<string>((resolve, reject) => {
+        let reader = new FileReader()
+        reader.onload = () => {
+            resolve(reader.result as string)
+        }
+        reader.onerror = () => {
+            reject("error")
+        }
+        reader.readAsDataURL(blob)
+    })
+}
